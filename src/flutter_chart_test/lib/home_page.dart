122b96819b95bca6e5dd3c1ace66eb27a3c17149
jbrito2507 <dev@pawiis.com>
2022-03-25 15:09:19 -0400

    se coloca dialog

--- src/flutter_chart_test/lib/home_page.dart
+++ src/flutter_chart_test/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chart_test/model/pie_section.dart';
+import 'package:flutter_chart_test/screen_dialog.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -23,11 +24,30 @@ class _HomePageState extends State<HomePage> {
               child: Container(
             color: Colors.green,
           )),
-          Expanded(child: ListView.builder(itemCount: sections.length, itemBuilder: (context, index) {}))
+          Expanded(
+              child: ListView.builder(
+                  itemCount: sections.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.green,
+                    );
+                  }))
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          final newSection = await showDialog<PieSection>(
+              context: context,
+              builder: (ctx) {
+                return const ScreenDialog();
+              });
+
+          if (newSection != null) {
+            setState(() {
+              sections.add(newSection);
+            });
+          }
+        },
         child: const Icon(Icons.add),
       ),
     );
---
+++ src/flutter_chart_test/lib/screen_dialog.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chart_test/model/pie_section.dart';
+
+class ScreenDialog extends StatefulWidget {
+  const ScreenDialog({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenDialog> createState() => _ScreenDialogState();
+}
+
+class _ScreenDialogState extends State<ScreenDialog> {
+  final _formKey = GlobalKey<FormState>();
+  String title = "";
+  String valor = "";
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Agregar Pais', style: TextStyle(fontSize: 20)),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Titulo',
+                  ),
+                  onChanged: (String value) {
+                    title = value;
+                    print(value);
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Valor',
+                  ),
+                  onChanged: (String value) {
+                    valor = value;
+                    print(value);
+                  }),
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            if (_formKey.currentState?.validate() ?? false) {
+              final new_section = PieSection(title, double.parse(valor));
+              Navigator.pop(context, new_section);
+            }
+          },
+          child: const Text('OK'),
+        ),
+      ],
+    );
+  }
+}
